4acbab6cb8c16232524cd648ee01af484c5c635a
nancylee713 <srlee713@gmail.com>
2019-12-17 10:19:04 -0700

    Add jquery get method

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,9 +2,11 @@ import { expect } from "chai"
 import sinon from "sinon"
 
 describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
+  describe("When the user comes to the page", () => {
+    it("I see the top word and its count on the right side of the page", () => {
+      // stub out http request
+      const top_word = sinon.stub().returns('chicken, 129');
+      expect(top_word).toEqual('chicken, 129');
     })
   })
 })
-
