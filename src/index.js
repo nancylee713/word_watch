fe52c9f879969835f0301e9052c6bc98d1250365
nancylee713 <srlee713@gmail.com>
2019-12-17 10:19:59 -0700

    Add jquery get method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.get("https://wordwatch-api.herokuapp.com/api/v1/top_word", function(data){
+    $('.top-word-title').append(Object.keys(data['word'])[0]);
+    $('.word-count').append(Object.values(data['word'])[0]);
+  });
 })
