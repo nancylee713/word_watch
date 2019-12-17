301d4e219bff2e33f78ec0a4502f02c4b0160f52
nancylee713 <srlee713@gmail.com>
2019-12-17 10:30:51 -0700

    Add jquery post method

--- src/index.js
+++ src/index.js
@@ -5,4 +5,13 @@ $(document).ready(() => {
     $('.top-word-title').append(Object.keys(data['word'])[0]);
     $('.word-count').append(Object.values(data['word'])[0]);
   });
+
+  $("#btnSubmit").click(function(){
+    var input = $('textarea').val();
+    $.post("https://wordwatch-api.herokuapp.com/api/v1/words",
+    { word: { value: input } },
+    function(data){
+      alert('{ message: ' + input + ' added! }');
+    });
+  });
 })
