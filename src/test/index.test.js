a7ff470f1746b20de89ec776125ffd5eff117665
nancylee713 <srlee713@gmail.com>
2019-12-17 11:33:43 -0700

    Debug initial test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -6,7 +6,8 @@ describe("Word watch functions", () => {
     it("I see the top word and its count on the right side of the page", () => {
       // stub out http request
       const top_word = sinon.stub().returns('chicken, 129');
-      expect(top_word).toEqual('chicken, 129');
+      console.log(top_word)
+      expect(top_word).to.eql('chicken, 129');
     })
   })
 })
