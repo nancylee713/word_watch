e81af6bf3cb2844589d5524a914993c2de057cbb
nancylee713 <srlee713@gmail.com>
2019-12-17 11:14:00 -0700

    Add a new section to display user input

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 $(document).ready(() => {
   $.get("https://wordwatch-api.herokuapp.com/api/v1/top_word", function(data){
     $('.top-word-title').append(Object.keys(data['word'])[0]);
-    $('.word-count').append(Object.values(data['word'])[0]);
+    $('.top-word-count').append(Object.values(data['word'])[0]);
   });
 
   $("#btnSubmit").click(function(){
@@ -11,6 +11,7 @@ $(document).ready(() => {
     $.post("https://wordwatch-api.herokuapp.com/api/v1/words",
     { word: { value: input } },
     function(data){
+      $('.new-word').append("<p style='margin-bottom: 10px'>" + input + "</p>");
       alert('{ message: ' + input + ' added! }');
     });
   });
